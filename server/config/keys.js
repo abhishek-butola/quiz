2fcbd8d6b9ca197a8dd3af30120441598e9044e1
Abhishek Butola <abhishekbutola08@gmail.com>
2019-05-03 23:40:23 +0530

    fin

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,9 +1,7 @@
 module.exports = {
-  mongoURI:
-    "mongodb://172.25.0.15:27017/quiz",
-  secretOrKey: "secret"
+  mongoURI: 'mongodb://localhost:27017/quiz',
+  secretOrKey: 'secret'
 };
-
 
 // db.createUser({
 //   user: 'abhishek',
@@ -11,8 +9,6 @@ module.exports = {
 //   roles: [{ role: 'readWrite', db:'quiz_uu'}]
 // })
 
-
 //mongodb://abhishek1999:abhishek1999@ds119802.mlab.com:19802/notebook
-
 
 //mongodb://abhishek:abhishek@54.91.230.139:27017/quiz_uu
